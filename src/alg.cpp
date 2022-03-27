34fe920ab2700ee392d57457a0cdf4cb5f93903b
AleksandorMart <99171200+AleksandorMart@users.noreply.github.com>
2022-03-27 23:20:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,59 +1,62 @@
 // Copyright 2021 NNTU-CS
-void serch(int *arr, int len) {
-int t;
-for (int i = 0; i < len - 1; i++) {
-for (int j = 0; j < len - 1 - i; j++) {
-if (arr[j] > arr[j + 1]) {
-t = arr[j];
-arr[j] = arr[j + 1];
-arr[j + 1] = t;
-}}}}  
-int countPairs1(int *arr, int len, int value) {
+int nPairs1(int* arr, int len, int value) {
 int n = 0;
-serch(arr, len);
 for (int i = 0; i < len - 1; i++) {
 for (int j = i + 1; j < len; j++) {
-if (arr[i] + arr[j] == value){
-n++;
-}}}
-return n;
+if (arr[i] + arr[j] == value) n++;
 }
-int countPairs2(int *arr, int len, int value) {
-int n = 0;
-int i, j;
-serch(arr, len);
-i = 0;
-j = len - 1;
-while (i <= j) {
-for (int t = i + 1; t <= j; t++) {
-if(arr[i] + arr[t] == value && arr[i] < value && arr[t] < value) {
-n++;
-}}
-i++;
-for (int t = j - 1; t >= i; t--) {
-if (arr[j] + arr[t] == value && arr[j] < value && arr[t] < value) {
-n++;
-}}
-j--;
 }
 return n;
 }
-int countPairs3(int *arr, int len, int value) {
-serch(arr, len);
+int nPairs2(int* arr, int len, int value) {
 int n = 0;
-int s = 0;
-while (arr[s] * 2 < value || s < len) {
-s++;
+int q = len;
+int r = len - 1;
+for (int q = len - 1; arr[q] > value; q--) r--;
+for (int i = 0; i <= len; i++) {
+if (arr[i] > value / 2) break;
+for (int j = r; j > i; j--) {
+if (arr[i] + arr[j] == value) {
+n++;
 }
-for (int i = 0; i < s; i++) {
-for (int j = s; j < len; j++) {
-if (arr[i] + arr[j] == value) {
+if (arr[i] + arr[j] < value) break;
+}
+}
+return n;
+}
+int nPairs3(int* arr, int len, int value) {
+int n = 0;
+int q = len - 1;
+for (int i = 0; i < len - 1; i++) {
+bool result = false;
+int start = i + 1;
+int mid = (start + q) / 2;
+while (start <= q) {
+if (arr[mid] == value - arr[i]) {
 n++;
-}}}
-for (int i = s; i < len - 1; i++) {
-for (int j = i + 1; j < len; j++) {
-if (arr[i] + arr[j] == value) {
+result = true;
+break;
+}
+else if (arr[mid] > value - arr[i]) {
+q = mid - 1;
+}
+else if (arr[mid] < value - arr[i]) {
+start = mid + 1;
+}
+mid = (start + q) / 2;
+}
+int temp1 = mid;
+int temp2 = mid;
+if (result == true) {
+while (arr[temp1 - 1] == arr[mid] && temp1 - 1 != i) {
 n++;
-}}}
+temp1--;
+}
+while (arr[temp2 + 1] == arr[mid]) {
+n++;
+temp2++;
+}
+}
+}
 return n;
 }
